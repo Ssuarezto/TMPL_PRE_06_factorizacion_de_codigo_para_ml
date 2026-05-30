669f7c14eaa34ce479755e7abe1946084008b1ba
Ssuarezto <159150447+Ssuarezto@users.noreply.github.com>
2026-05-30 11:36:50 -0500

    Tarea taller 6
    Entrega taller 6

--- homework/save_model.py
+++ homework/save_model.py
@@ -4,7 +4,8 @@ import pickle
 
 def save_model(model, save_path="models/estimator.pkl"):
     """Save the model to the specified path."""
-    if not os.path.exists("models"):
-        os.makedirs("models_")
+    directory = os.path.dirname(save_path)
+    if directory and not os.path.exists(directory):
+        os.makedirs(directory, exist_ok=True)
     with open(save_path, "wb") as file:
         pickle.dump(model, file)
