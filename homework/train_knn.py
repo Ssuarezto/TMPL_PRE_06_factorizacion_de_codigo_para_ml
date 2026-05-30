e2061efa9dcf8267de0fccdbf780296a831e187f
Ssuarezto <159150447+Ssuarezto@users.noreply.github.com>
2026-05-30 10:26:21 -0500

    Entrega taller 6.

--- homework/train_knn.py
+++ homework/train_knn.py
@@ -21,7 +21,7 @@ x = df.copy()
 x.pop("quality")
 
 # dividir los datos en entrenamiento y testing
-(x_train, x_test, y_train, y_test) = train_test_split(
+x_train, x_test, y_train, y_test = train_test_split(
     x,
     y,
     test_size=0.25,
